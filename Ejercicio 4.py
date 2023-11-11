3a7394060697c9e162e0a8215b7f71e709ed0a5f
bblandoesp <bblandoesp@educacion.navarra.es>
2023-11-11 23:24:11 +0100

    Datos sobre una persona

---
+++ Ejercicio 4.py
@@ -0,0 +1,6 @@
+Datos_del_usuario = {}
+while True:
+    sobre_ti = input ("Datos de la persona:")
+    Respuestas = input ("Llena la informacion sobre el dato de la persona:")
+    Datos_del_usuario[sobre_ti] = Respuestas
+    print (Datos_del_usuario)
